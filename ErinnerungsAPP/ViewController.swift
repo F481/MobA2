7843b9946209253327c90ab8ba988ab83e6f9caf
F481 <fabi.kcirf@gmail.com>
2017-07-08 19:20:37 +0200

    save reminder to coredata

--- ErinnerungsAPP/ViewController.swift
+++ ErinnerungsAPP/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
 
     // array instance variable that can be accessed from and method in that class. We use it to pass the categories to the PickerView.
     var categories: [String] = [String]()
-    var selectedCategory: String = ""
+    // select the last entry by default (if there is no change to the pick viewer we will get a correct value
+    var selectedCategory: Int = 3
     
     @IBOutlet weak var reminderNameField: UITextField!
     @IBOutlet weak var reminderDate: UIDatePicker!
@@ -29,8 +30,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         // fill the array with the categories we want
         categories = ["Geburtstag", "Jahrestag", "Hochzeit", "Valentinstag"]
-        // select the first entry by default (if there is no change to the pick viewer we will get a correct value
-        selectedCategory = categories[0]
+        
+        // set default to last entry, because it looks nicer if the margin at top isn't so large at the beginning.
+        categoryPicker.selectRow(3, inComponent:0, animated:true)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,10 +60,21 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         // This method is triggered whenever the user makes a change to the picker selection.
         // The parameter named row and component represents what was selected.
-        selectedCategory = categories[row]
+        selectedCategory = row
     }
     
     @IBAction func saveReminderPressed(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let reminder = Reminder(context: context) // link reminder and context
+        reminder.name = reminderNameField.text!
+        reminder.date = reminderDate.date as NSDate
+        reminder.catecory = Int16(selectedCategory)
+        
+        // save to coredata
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        // move back to overview
+        let _ = navigationController?.popViewController(animated: true)
+        
         print(reminderNameField.text!)
         print(reminderDate.date)
         print(selectedCategory)
